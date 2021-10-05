386eb8f5a42c84013cf5ae1d90507051c3aa709d
Karthick <ramkarthick18@gmail.com>
2021-10-05 13:33:59 +0530

    initial commit

---
+++ src/main/java/com/worldingroup/worldin/WorldinApplication.java
@@ -0,0 +1,22 @@
+package com.worldingroup.worldin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WorldinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldinApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello()   
+	{  
+		return "<h1>Hello from Worldin, we're up and running...</h1>";  
+	}
+
+}
---
+++ src/test/java/com/worldingroup/worldin/WorldinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldingroup.worldin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
